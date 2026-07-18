7e52a92a4d74816af16312d034ded5dbdfb23a1d
liangk505 <2132543827@qq.com>
2026-07-18 11:02:24 -0700

    imx6ull官方SDK移植完成点灯

---
+++ Board_Drivers/4_ledc_sdk/main.c
@@ -0,0 +1,57 @@
+#include "fsl_iomuxc.h"
+#include "MCIMX6Y2.h"
+
+void Clock_Init(void)
+{
+    CCM->CCGR2 = 0xFFFFFFFF;
+}
+void LED_Init(void)
+{
+    IOMUXC_SetPinMux(IOMUXC_GPIO1_IO03_GPIO1_IO03,0);
+    IOMUXC_SetPinConfig(IOMUXC_GPIO1_IO03_GPIO1_IO03,0x10B0);
+    GPIO1->GDIR = 0x00000008;
+    GPIO1->DR = 0x00000008;     //初始LED为关
+}
+
+void LED_ON(void)
+{
+    GPIO1->DR &= ~(1<<3);
+}
+
+void delay_short(volatile unsigned int n)
+{
+    while(n--)
+    {
+
+    }
+}
+
+void delay(volatile unsigned int n)
+{
+    while(n--)
+    {
+        delay_short(0x7ff);
+    }
+}
+
+void LED_OFF(void)
+{
+    GPIO1->DR |= (1<<3);
+}
+
+int main()
+{
+    Clock_Init();
+    LED_Init();
+
+
+    while(1)
+    {
+        LED_OFF();
+        delay(500);
+        LED_ON();
+        delay(500);
+
+    }
+    return 0;
+}
